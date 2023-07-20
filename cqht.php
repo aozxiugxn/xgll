2e5723f278a3c98d57f731e3a553d4805c9dd6c0
aozxiugxn <139823738+aozxiugxn@users.noreply.github.com>
2023-07-20 22:39:45 +0800

    Create cqht.php

---
+++ cqht.php
@@ -0,0 +1,10 @@
+<?php
+  $zz=$_GET["zz"];
+  if($zz=="无至尊"){
+  file_put_contents("zz.txt","0");
+  echo '<script language="javascript"> alert("无至尊修改成功");</script>';
+}
+if($zz=="至尊9"){
+file_put_contents("zz.txt","QPe7/0h+JlipmrPu6bo1JAQoEBiq40nIjWRbh8FMwbCQiVZ7+2SwMtrFFef9qX4uqlUNeY/f6rhHhoGxfmhDgvQydV0hCJxKA+w1tUz4i96iHOJEiVGznmPfjwhMeo+omUIAKn3Bmnc=");
+echo '<script language="javascript"> alert("至尊9修改成功");</script>';
+?>
